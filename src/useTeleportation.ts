94c5fc352a9de4e1584752e040251b0c8ffc7ba4
Richard Anaya <richard.anaya@gmail.com>
2023-03-12 17:48:46 -0700

    fix

--- src/useTeleportation.ts
+++ src/useTeleportation.ts
@@ -1,4 +1,5 @@
 import { useThree } from "@react-three/fiber";
+import { useXR } from "@react-three/xr";
 import { useCallback, useEffect, useState } from "react";
 import { Quaternion, Vector3 } from "three";
 
@@ -6,6 +7,7 @@ export function useTeleportation() {
   const [baseReferenceSpace, setBaseReferenceSpace] =
     useState<XRReferenceSpace | null>(null);
   const gl = useThree((state) => state.gl);
+  const { session } = useXR();
 
   useEffect(() => {
     const b = gl.xr.getReferenceSpace();
@@ -24,12 +26,29 @@ export function useTeleportation() {
   }, [gl]);
 
   const teleportTo = useCallback(
-    (worldPosition: Vector3, offsetRotation?: Quaternion) => {
-      if (baseReferenceSpace) {
+    async (worldPosition: Vector3, offsetRotation?: Quaternion) => {
+      if (baseReferenceSpace && session) {
+        debugger;
+        const pose = await new Promise<XRViewerPose | undefined>((resolve) => {
+          // @ts-ignore
+          session.requestAnimationFrame((_, xrFrame) => {
+            const pose = xrFrame.getViewerPose(baseReferenceSpace);
+            resolve(pose);
+          });
+        });
+        debugger;
+        if (!pose) return;
+
+        const offsetX = pose?.transform.position.x || 0;
+        const offsetY = pose?.transform.position.y || 0;
+        const offsetZ = pose?.transform.position.z || 0;
+
+        console.log(offsetX, offsetY, offsetZ);
+
         const offsetFromBase = {
-          x: -worldPosition.x,
+          x: -(worldPosition.x + offsetX),
           y: -worldPosition.y,
-          z: -worldPosition.z,
+          z: -(worldPosition.z + offsetZ),
         };
         const transform = new XRRigidTransform(
           offsetFromBase,
